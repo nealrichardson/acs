cc6f9bc18b65e79a6f2e196d4cdbfc4693dcc44f
Neal Richardson <neal@crunch.io>
2017-02-26 21:54:17 -0800

    Lint code with formatR. Add .Rprofile/options support for API key

---
+++ NEWS.md
@@ -0,0 +1,4 @@
+# What's changed
+
+* Code linting using [formatR](https://yihui.name/formatr/)
+* Allow setting API key in `options` (and thus in .Rprofile) under 'census.api.key'
--- tests/testthat/helper.R
+++ tests/testthat/helper.R
@@ -1,4 +1,2 @@
 Sys.setlocale("LC_COLLATE", "C") ## What CRAN does; affects sort order
 set.seed(999) ## To ensure that tests that involve randomness are reproducible
-
-acs.fetch <- function (...) acs::acs.fetch(..., key=getOption("census.api.key"))
